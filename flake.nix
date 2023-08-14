08d92d1711c32811501287cc7b851ab2a6694bed
mehbark <terezi@pyrope.net>
2023-08-14 07:11:18 -0400

    flake time?

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    inputs.home-manager.url = "github:nix-community/home-manager";
+    inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    outputs = {self, nixpkgs, home-manager }: {
+        nixosConfigurations = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                ./configuration.nix
+            ];
+        }
+    }
+}
