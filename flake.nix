d1f9e2c3ec6e3dd4f8311fda3a44850e97ce6974
mehbark <terezi@pyrope.net>
2023-08-14 07:45:15 -0400

    maybe

--- flake.nix
+++ flake.nix
@@ -1,16 +1,24 @@
 {
-    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    inputs.home-manager.url = "github:nix-community/home-manager";
-    inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  description = "mehbark's NixOS configuration";
 
-    outputs = {self, nixpkgs, home-manager }: {
-        nixosConfigurations = {
-            nix = nixpkgs.lib.nixosSystem {
-                system = "x86_64-linux";
-                modules = [
-                    ./configuration.nix
-                ];
-            };
-        };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = inputs@{ self, nixpkgs, home-manager }: {
+    nixosConfigurations = {
+      nix = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.mbk = import ./home.nix;
+          }
+        ];
+      };
     };
+  };
 }
